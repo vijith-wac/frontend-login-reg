f3b52ddb1136446a03577f4470c1d8ba67f9b86e
vijith.p_wac_react <vijith.p@webandcrafts.com>
2025-07-25 10:21:16 +0530

    home add

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import Login from './pages/login';
 import Register from './pages/register';
 import Profile from './pages/Profile';
 import ProtectedRoute from './components/ProtectedRoute';
+import Home from './components/Home';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='/' element={<Home/>} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         {/* <Route path="/profile" element={<Profile />} /> */}
---
+++ src/components/Home.jsx
@@ -0,0 +1,8 @@
+const Home = ()=>{
+    return(
+        <div>
+            <h2>Home Page</h2>
+        </div>
+    )
+}
+export default Home
